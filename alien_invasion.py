247b9dc94f4cbb8660d2d6d9222ffdcbb01ff5e6
Matthew <matthewyungisworking@gmail.com>
2023-06-26 12:31:52 -0700

    created screen for game

---
+++ alien_invasion.py
@@ -0,0 +1,30 @@
+import sys 
+
+import pygame
+from settings import Settings
+
+class AlienInvasion:
+    """Master class"""
+    def __init__(self):
+        """Init game"""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.bg_color = (230,230,230)
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            self.screen.fill(self.settings.bg_color)
+            #makes the most recently drawn screen visible 
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
